6ee519b5bfe37cb6132c22888cdd137399ec7e35
qijinliang <15914115336@163.com>
2020-09-14 10:45:39 +0800

    二叉补充

--- src/com/二叉堆/Heap.java
+++ src/com/二叉堆/Heap.java
@@ -43,7 +43,23 @@ public class Heap {
      * @param length      堆的有效大小
      */
     public static void downAdjust(int[] array, int parentIndex, int length) {
-
+        //temp 保存父节点值，用于最后的赋值
+        int temp = array[parentIndex];
+        int childIndex = 2 * parentIndex + 1;
+        while (childIndex < length) {
+            //如果有右节点，且右节点小于左节点的值，则定位到右节点
+            if (childIndex + 1 < length && array[childIndex] > array[childIndex + 1]) {
+                childIndex++;
+            }
+            //如果父节点小于任何一个孩子的值，则直接跳出
+            if (temp <= array[childIndex])
+                break;
+            //无须真正交换，单向赋值即可
+            array[parentIndex] = array[childIndex];
+            parentIndex = childIndex;
+            childIndex = 2 * childIndex + 1;
+        }
+        array[parentIndex] = temp;
     }
 
     public static void main(String[] args) {
